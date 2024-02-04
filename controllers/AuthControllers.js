023b3771a42e151467d0a212142e1d5377a099df
NishantRajhans <nishantrajhans35@gmail.com>
2024-02-04 14:22:34 +0530

    update email url

--- controllers/AuthControllers.js
+++ controllers/AuthControllers.js
@@ -2,25 +2,28 @@ const express = require("express");
 const UserSchema = require("../schema/UserSchema");
 const JWT = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-const nodemailer=require("nodemailer");
+const nodemailer = require("nodemailer");
 require("dotenv").config();
-const Sendmail=async(FirstName,Email,UserId)=>{
-  try{
-    const transporter= nodemailer.createTransport({
-      host:process.env.MAIL_HOST,
-      auth:{
-        user:process.env.MAIL_USER,
-        pass:process.env.MAIL_PASS
-      }
-    })
-    const hashId=JWT.sign({
-      UserId
-    },process.env.JWT_SECRET_KEY);
-    let info=await transporter.sendMail({
-      from:'Nishant Priyadarshi',
-      to:Email,
-      subject:"For Varification of Email",
-      html:`
+const Sendmail = async (FirstName, Email, UserId) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+    });
+    const hashId = JWT.sign(
+      {
+        UserId,
+      },
+      process.env.JWT_SECRET_KEY
+    );
+    let info = await transporter.sendMail({
+      from: "Nishant Priyadarshi",
+      to: Email,
+      subject: "For Varification of Email",
+      html: `
       <!DOCTYPE html>
 <html>
 <head>
@@ -210,7 +213,8 @@ const Sendmail=async(FirstName,Email,UserId)=>{
                     <table border="0" cellpadding="0" cellspacing="0">
                       <tr>
                         <td align="center" bgcolor="#1a82e2" style="border-radius: 6px;">
-                          <a href="https://bitolx-backend.onrender.com/api/v1/Auth/VerifyUser/${hashId} target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">Verify Email</a>
+                          <a href="http://localhost:3000/api/v1/Auth/VerifyUser/${hashId}" 
+                          target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">Verify Email</a>
                         </td>
                       </tr>
                     </table>
@@ -261,8 +265,7 @@ const Sendmail=async(FirstName,Email,UserId)=>{
           <!-- start unsubscribe -->
           <tr>
             <td align="center" bgcolor="#e9ecef" style="padding: 12px 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 20px; color: #666;">
-              <p style="margin: 0;">To stop receiving these emails, you can <a href="https://sendgrid.com" target="_blank">unsubscribe</a> at any time.</p>
-              <p style="margin: 0;">Paste 1234 S. Broadway St. City, State 12345</p>
+              <p style="margin: 0;">To stop receiving these emails, you can <p>unsubscribe BitOlx</p> at any time.</p>
             </td>
           </tr>
           <!-- end unsubscribe -->
@@ -282,61 +285,64 @@ const Sendmail=async(FirstName,Email,UserId)=>{
 
 </body>
 </html>
-      `
-    })
-  }catch(err){
-    console.log("Error while sending Email",err)
+      `,
+    });
+  } catch (err) {
+    console.log("Error while sending Email", err);
   }
-}
+};
 exports.LogIn = async (req, res) => {
-    try{
-        const {Email,Password,ConfirmPassword}=req.body;
-        if(!Email ||!Password || !ConfirmPassword){
-            return res.status(200).json({
-                success: false,
-                message: "All fields are required",
-              });
-        }
-        if(Password!==ConfirmPassword){
-            return res.status(200).json({
-                success: false,
-                message: "Password and Confirm Password do not match",
-              });
-        }
-        const User=await UserSchema.findOne({Email:Email})
-        if(!User){
-            return res.status(200).json({
-                success: false,
-                message: "User does not exist",
-              });
-        }
-        if(!User.Verified){
-          return res.status(200).json({
-            success: false,
-            message: "User does not verified",
-          });
-        }
-        if(! await bcrypt.compare(Password, User.Password)){
-            return res.status(200).json({
-                success: false,
-                message: "Password is incorrect",
-              });
-        }
-        const Token= await JWT.sign({
-            Email:Email,
-            Password:Password,
-        },process.env.JWT_SECRET_KEY)
-        User.Token=Token
-        User.Password=undefined
-        return res.status(200).json({
-            success: true,
-            message: "User LogIn successfully",
-            User:User
-          });
-    }catch(err){
-        console.log(err);
-    return res.status(200).json({ success: false, message: "Error In LogIn" });
+  try {
+    const { Email, Password, ConfirmPassword } = req.body;
+    if (!Email || !Password || !ConfirmPassword) {
+      return res.status(200).json({
+        success: false,
+        message: "All fields are required",
+      });
     }
+    if (Password !== ConfirmPassword) {
+      return res.status(200).json({
+        success: false,
+        message: "Password and Confirm Password do not match",
+      });
+    }
+    const User = await UserSchema.findOne({ Email: Email });
+    if (!User) {
+      return res.status(200).json({
+        success: false,
+        message: "User does not exist",
+      });
+    }
+    if (!User.Verified) {
+      return res.status(200).json({
+        success: false,
+        message: "User does not verified",
+      });
+    }
+    if (!(await bcrypt.compare(Password, User.Password))) {
+      return res.status(200).json({
+        success: false,
+        message: "Password is incorrect",
+      });
+    }
+    const Token = await JWT.sign(
+      {
+        Email: Email,
+        Password: Password,
+      },
+      process.env.JWT_SECRET_KEY
+    );
+    User.Token = Token;
+    User.Password = undefined;
+    return res.status(200).json({
+      success: true,
+      message: "User LogIn successfully",
+      User: User,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(200).json({ success: false, message: "Error In LogIn" });
+  }
 };
 exports.SignUp = async (req, res) => {
   try {
@@ -374,7 +380,7 @@ exports.SignUp = async (req, res) => {
         message: "User already exists",
       });
     }
-    const hashedPassword = await bcrypt.hash(Password,10);
+    const hashedPassword = await bcrypt.hash(Password, 10);
     const NewUser = await UserSchema.create({
       FirstName: FirstName,
       LastName: LastName,
@@ -383,32 +389,64 @@ exports.SignUp = async (req, res) => {
       Password: hashedPassword,
       Verified: false,
     });
-    Sendmail(FirstName,Email,NewUser._id);
+    Sendmail(FirstName, Email, NewUser._id);
     return res
       .status(200)
-      .json({ success: true, message: "Email send Successfully" ,NewUser:NewUser});
+      .json({
+        success: true,
+        message: "Email send Successfully",
+        NewUser: NewUser,
+      });
   } catch (err) {
     console.log(err);
     return res.status(200).json({ success: false, message: "Error In SignUp" });
   }
 };
-exports.VerifyUser=async(req,res)=>{
-    try{
-      const HashedId=req.params.id;
-      const UserId= JWT.verify(HashedId,process.env.JWT_SECRET_KEY)
-      const UpdateUser=await UserSchema.findByIdAndUpdate(UserId.UserId,{
-        $set:{
-          Verified:true
-        }
-      },{new:true})
-        return res.status(200).json({
-            success:true,
-            message:"User verified successfully",
-            UpdateUser:UpdateUser
-        })
-
-    }catch(err){
-        console.log(err);
-    return res.status(200).json({ success: false, message: "Error In VerifyUser" });
-    }
-}
+exports.VerifyUser = async (req, res) => {
+  try {
+    const HashedId = req.params.id;
+    const UserId = JWT.verify(HashedId, process.env.JWT_SECRET_KEY);
+    const UpdateUser = await UserSchema.findByIdAndUpdate(
+      UserId.UserId,
+      {
+        $set: {
+          Verified: true,
+        },
+      },
+      { new: true }
+    );
+    return res.status(200)({
+      html: `<div id='card' class="animated fadeIn">
+  <div id='upper-side'>
+    <?xml version="1.0" encoding="utf-8"?>
+      <!-- Generator: Adobe Illustrator 17.1.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+      <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+      <svg version="1.1" id="checkmark" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" xml:space="preserve">
+        <path d="M131.583,92.152l-0.026-0.041c-0.713-1.118-2.197-1.447-3.316-0.734l-31.782,20.257l-4.74-12.65
+	c-0.483-1.29-1.882-1.958-3.124-1.493l-0.045,0.017c-1.242,0.465-1.857,1.888-1.374,3.178l5.763,15.382
+	c0.131,0.351,0.334,0.65,0.579,0.898c0.028,0.029,0.06,0.052,0.089,0.08c0.08,0.073,0.159,0.147,0.246,0.209
+	c0.071,0.051,0.147,0.091,0.222,0.133c0.058,0.033,0.115,0.069,0.175,0.097c0.081,0.037,0.165,0.063,0.249,0.091
+	c0.065,0.022,0.128,0.047,0.195,0.063c0.079,0.019,0.159,0.026,0.239,0.037c0.074,0.01,0.147,0.024,0.221,0.027
+	c0.097,0.004,0.194-0.006,0.292-0.014c0.055-0.005,0.109-0.003,0.163-0.012c0.323-0.048,0.641-0.16,0.933-0.346l34.305-21.865
+	C131.967,94.755,132.296,93.271,131.583,92.152z" />
+        <circle fill="none" stroke="#ffffff" stroke-width="5" stroke-miterlimit="10" cx="109.486" cy="104.353" r="32.53" />
+      </svg>
+      <h3 id='status'>
+      Success
+    </h3>
+  </div>
+  <div id='lower-side'>
+    <p id='message'>
+      Congratulations, your account has been successfully created.
+    </p>
+    <a href="http://localhost:3000/LogIn" id="contBtn">Log In</a>
+  </div>
+</div>`,
+    });
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(200)
+      .json({ success: false, message: "Error In VerifyUser" });
+  }
+};
